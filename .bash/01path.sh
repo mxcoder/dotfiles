058a0aa000f389520212ac7436c534255f4070d5
Ricardo Vega <ricardoe@gmail.com>
2014-03-26 18:36:53 -0600

    Fixing node

--- .bash/01path.sh
+++ .bash/01path.sh
@@ -3,6 +3,10 @@
 # Work dir
 WORKHOME=$HOME/Work
 export PATH="$WORKHOME:$PATH"
+
+# NPM / Node
+export PATH="$HOME/.npm/bin:$PATH"
+
 # Java
 JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre
 export PATH="$JAVA_HOME/bin:$PATH"
